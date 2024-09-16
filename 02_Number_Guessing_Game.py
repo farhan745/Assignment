c37854d0fa18a23a8054999bb2c5ab9b541027ae
Farhan Zawad <145384778+farhan745@users.noreply.github.com>
2024-09-17 01:53:45 +0600

    Update 02_Number_Guessing_Game.py

--- 02_Number_Guessing_Game.py
+++ 02_Number_Guessing_Game.py
@@ -4,14 +4,14 @@ count = 0
 while True:
   choice_your_num = int(input("Enter your guess: "))
   computer_num = computer_choice_number.randint(1,100)
-  if computer_num==choice_your_num:
-    count+=1
+  if computer_num == choice_your_num:
+    count += 1
     print(f"Congratulations! You've guessed the number in {count} attempts.")
     break
-  elif computer_num>choice_your_num:
+  elif computer_num > choice_your_num:
     print("Too low!")
-    count+=1
+    count += 1
   else:
     print("Too high!")
-    count+=1
+    count += 1
   
